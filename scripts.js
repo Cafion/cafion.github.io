c50ce50f2964444b3b8241d8fb5f9fed500942af
Cafion <mrcode2275@gmail.com>
2025-10-28 15:19:56 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Get references to the image and button
+const img = document.getElementById("BABYBONO");
+const btn = document.getElementById("changeImageBtn");
+
+// Define the image sources
+const image1 = "BABYBONO.jpg";     // the current image shown
+const image2 = "DEFAULTDUDE.jpg";  // the image you want to switch to
+
+// Track which image is currently displayed
+let showingImage1 = true;
+
+// Add an event listener for the button click
+btn.addEventListener("click", () => {
+  if (showingImage1) {
+    img.src = image2; // switch to DEFAULTDUDE
+    btn.textContent = "Change Back";
+  } else {
+    img.src = image1; // switch back to BABYBONO
+    btn.textContent = "Change Image";
+  }
+
+  // Flip the state for next time
+  showingImage1 = !showingImage1;
+});
