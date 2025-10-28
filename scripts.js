6980acc634e1a97c6777853797f54505a4939ad4
Cafion <mrcode2275@gmail.com>
2025-10-28 19:21:12 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ const img = document.getElementById("BABYBONO");
 const btn = document.getElementById("changeImageBtn");
 
 // Define your image paths
-const image1 = "BABYBONO";   // the one you want to show by default
-const image2 = "DEFAULTDUDE"; // the other one
+const image1 = "BABYBONO.jpg";   // the one you want to show by default
+const image2 = "DEFAULTDUDE.jpg"; // the other one
 
 //  Set the default image when the page loads
 img.src = image1;
